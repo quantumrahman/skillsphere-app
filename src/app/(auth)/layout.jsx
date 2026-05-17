bd1574c985a282ca268a31272489f2787c3618ec
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-18 01:12:17 +0600

    feat: implement auth footer

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -16,6 +16,9 @@ const AuthLayout = ({ children }) => {
             className={`${bricolageGrotesque.variable} w-full min-h-screen relative antialiased`}
         >
             {children}
+            <div className='absolute bottom-5 left-0 right-0 text-center'>
+                <span className='text-sm text-[#ffffff]'>&copy; {new Date().getFullYear()} SkillSphere. By Rakibul Rahman</span>
+            </div>
         </div>
     );
 };

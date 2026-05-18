f88d025676a7889d3815d66f37f67a9c9e142341
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-18 11:39:42 +0600

    feat: create update form and implement update profile page

---
+++ src/components/ui/UpdateForm/UpdateForm.jsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { updateSchema } from '@/schema/updateSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { AlertCircle } from '@boxicons/react';
+
+const UpdateForm = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: zodResolver(updateSchema),
+    });
+
+    const handleOnSubmit = (data) => {
+        console.log(data);
+    };
+
+    return (
+        <form onSubmit={handleSubmit(handleOnSubmit)}>
+            <div className='space-y-1.5'>
+                <div className='w-full space-y-1.5'>
+                    <input
+                        type='text'
+                        aria-label='input'
+                        placeholder='Enter your name'
+                        name='name'
+                        className={`w-full py-3 px-4 bg-[#1e1e1e]/30 border rounded-full text-sm font-normal text-[#ffffff] caret-[#ff851b] placeholder:text-[#ffffff]/30 focus:outline-none ${errors?.name ? 'border-red-500' : 'border-[#1e1e1e] focus:border-[#ff851b]'}`}
+                        {...register('name')}
+                    />
+                    <div className='w-full h-5'>
+                        {errors?.name && (
+                            <span className='flex items-center gap-1 text-sm text-red-500 justify-end'>
+                                <AlertCircle pack='filled' color='red' size='xs' />
+                                {errors?.name?.message}
+                            </span>
+                        )}
+                    </div>
+                </div>
+                <div className='w-full space-y-1.5'>
+                    <input
+                        type='text'
+                        aria-label='input'
+                        placeholder='Enter your photo url'
+                        name='photo'
+                        className={`w-full py-3 px-4 bg-[#1e1e1e]/30 border rounded-full text-sm font-normal text-[#ffffff] caret-[#ff851b] placeholder:text-[#ffffff]/30 focus:outline-none ${errors?.photo ? 'border-red-500' : 'border-[#1e1e1e] focus:border-[#ff851b]'}`}
+                        {...register('photo')}
+                    />
+                    <div className='w-full h-5'>
+                        {errors?.photo && (
+                            <span className='flex items-center gap-1 text-sm text-red-500 justify-end'>
+                                <AlertCircle pack='filled' color='red' size='xs' />
+                                {errors?.photo?.message}
+                            </span>
+                        )}
+                    </div>
+                </div>
+                <button
+                    type='submit'
+                    aria-label='button'
+                    role='button'
+                    className='w-full py-3 px-4 bg-[#ff851b] border border-[#ff851b] rounded-full text-sm font-semibold text-[#000000] cursor-pointer transition-all duration-200 ease-in-out hover:bg-[#ff8b26]'
+                >
+                    Save Changes
+                </button>
+            </div>
+        </form>
+    );
+};
+
+export default UpdateForm;

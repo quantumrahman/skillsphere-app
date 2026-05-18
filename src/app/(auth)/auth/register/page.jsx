bd61f5a7f652d406e27f4fc3dc668ac45232d642
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-18 10:15:36 +0600

    feat: implement social login in register page

--- src/app/(auth)/auth/register/page.jsx
+++ src/app/(auth)/auth/register/page.jsx
@@ -1,8 +1,7 @@
-import { FcGoogle } from 'react-icons/fc';
-
 import Image from 'next/image';
 import Link from 'next/link';
 import RegisterForm from '@/components/ui/RegisterForm/RegisterForm';
+import GoogleButton from '@/components/ui/GoogleButton/GoogleButton';
 
 const RegisterPage = () => {
     return (
@@ -32,18 +31,7 @@ const RegisterPage = () => {
                         </Link>
                     </span>
                 </div>
-                <div className='w-full flex items-center justify-center gap-3'>
-                    <div className='w-full h-px bg-[#1e1e1e]'></div>
-                    <button
-                        type='button'
-                        aria-label='button'
-                        role='button'
-                        className='p-2.5 bg-transparent border border-[#1e1e1e] rounded-full cursor-pointer transition-all duration-300 ease-in-out hover:bg-[#1e1e1e]/30'
-                    >
-                        <FcGoogle size={20} />
-                    </button>
-                    <div className='w-full h-px bg-[#1e1e1e]'></div>
-                </div>
+                <GoogleButton />
             </div>
         </div>
     );

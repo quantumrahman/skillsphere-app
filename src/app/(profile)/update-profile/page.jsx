88d03196f67a7f723266107134eb358bb4c139fd
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-18 11:40:23 +0600

    feat: implement update form

--- src/app/(profile)/update-profile/page.jsx
+++ src/app/(profile)/update-profile/page.jsx
@@ -1,5 +1,18 @@
+import UpdateForm from "@/components/ui/UpdateForm/UpdateForm";
+
 const UpdateProfilePage = () => {
-    return <div></div>;
+    return (
+        <div className='w-full min-h-screen flex items-center justify-center px-5'>
+            <div className='w-full max-w-[350px]'>
+                <div className="w-full text-center">
+                    <h3 className="text-base font-semibold text-[#ffffff] sm:text-lg md:text-xl">Update Profile</h3>
+                </div>
+                <div className="w-full mt-12">
+                    <UpdateForm />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default UpdateProfilePage;

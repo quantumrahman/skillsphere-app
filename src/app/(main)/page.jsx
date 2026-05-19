6e3bf40fe4b07944936f9e25137960bdb307385d
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-19 19:43:28 +0600

    feat: implement learning tips section

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,9 +1,12 @@
+import LearningTips from "@/components/layouts/LearningTips/LearningTips";
 import PopularCourses from "@/components/layouts/PopularCourses/PopularCourses";
 
 const HomePage = () => {
     return (
         <div className="w-full">
             <PopularCourses />
+            <div className="w-full h-px bg-[#1e1e1e]"></div>
+            <LearningTips />
         </div>
     )
 };

4cdff5902e6b53cfffb6ed2e8ccd83acb4e430da
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-20 10:41:17 +0600

    feat: create learning card and implement learning tip list container

---
+++ src/components/ui/LearingCard/LearningCard.jsx
@@ -0,0 +1,19 @@
+const LearningCard = ({ title = '', paragraph = '', icon, className = '' }) => {
+    return (
+        <div
+            className={`w-full p-8 md:p-10 bg-[#1e1e1e]/30 rounded-xl relative overflow-hidden ${className}`}
+        >
+            <div className='space-y-4 relative inset-0 z-10'>
+                <h2 className='text-2xl font-bold text-[#ffffff] md:text-3xl'>
+                    {title}
+                </h2>
+                <p className='text-base text-[#A3A3A3]'>{paragraph}</p>
+            </div>
+            <div className='absolute top-1/2 right-0 transform -translate-y-1/2 md:translate-x-1/2'>
+                {icon}
+            </div>
+        </div>
+    );
+};
+
+export default LearningCard;
--- src/components/ui/LearningTipsList/LearningTipsList.jsx
+++ src/components/ui/LearningTipsList/LearningTipsList.jsx
@@ -1,39 +1,34 @@
+import { Core, Reading, Timer } from '@boxicons/react';
+import LearningCard from '../LearingCard/LearningCard';
+
 const LearningTipsList = () => {
     return (
         <div className='w-full max-w-[1100px] mx-auto grid grid-cols-1 gap-8 md:grid-cols-2'>
-            <div className='w-full p-8 md:p-10 bg-[#1e1e1e]/30 rounded-xl space-y-4'>
-                <h2 className='text-2xl font-bold text-[#ffffff] md:text-3xl'>
-                    Smart Study Techniques
-                </h2>
-                <p className='text-base text-[#A3A3A3]'>
-                    Using effective study techniques helps you learn more in
-                    less time. Methods like active recall, spaced repetition,
-                    and summarizing notes improve understanding and strengthen
-                    long-term memory.
-                </p>
-            </div>
-            <div className='w-full p-8 md:p-10 bg-[#1e1e1e]/30 rounded-xl space-y-4'>
-                <h2 className='text-2xl font-bold text-[#ffffff] md:text-3xl'>
-                    Master Your Time
-                </h2>
-                <p className='text-base text-[#A3A3A3]'>
-                    Good time management helps you balance study and personal
+            <LearningCard
+                title='Master Your Time'
+                paragraph='Good time management helps you balance study and personal
                     life. Techniques like the Pomodoro method, daily planning,
                     and setting priorities make your time more productive and
-                    organized.
-                </p>
-            </div>
-            <div className='w-full p-8 md:p-10 bg-[#ff851b]/10 rounded-xl space-y-4 md:col-span-2'>
-                <h2 className='text-2xl font-bold text-[#ffffff] md:text-3xl'>
-                    Stay Consistent & Focused
-                </h2>
-                <p className='text-base text-[#A3A3A3]'>
-                    Consistency is key to achieving your goals. Creating a
+                    organized.'
+                icon={<Timer color='#1e1e1e' size='4xl' />}
+            />
+            <LearningCard
+                title='Smart Study Techniques'
+                paragraph='Using effective study techniques helps you learn more in
+                    less time. Methods like active recall, spaced repetition,
+                    and summarizing notes improve understanding and strengthen
+                    long-term memory.'
+                icon={<Reading color='#1e1e1e' size='4xl' />}
+            />
+            <LearningCard
+                title='Stay Consistent & Focused'
+                paragraph='Consistency is key to achieving your goals. Creating a
                     distraction-free environment and following a regular study
                     routine helps improve focus and ensures steady progress over
-                    time.
-                </p>
-            </div>
+                    time.'
+                icon={<Core color='#1e1e1e' size='4xl' />}
+                className='md:col-span-2'
+            />
         </div>
     );
 };

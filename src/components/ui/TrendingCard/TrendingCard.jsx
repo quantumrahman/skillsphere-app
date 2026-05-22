c32cf8f7188906e3748df22fee1aa9ca965fdf32
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-22 17:54:52 +0600

    feat: implement dynamic trending data and show ui

--- src/components/ui/TrendingCard/TrendingCard.jsx
+++ src/components/ui/TrendingCard/TrendingCard.jsx
@@ -1,19 +1,42 @@
 import { Calendar } from '@boxicons/react';
+import { format } from 'date-fns';
 
-const TrendingCard = () => {
+import Image from 'next/image';
+import Badge from '../Badge/Badge';
+
+const TrendingCard = ({ course }) => {
     return (
-        <div className='w-full p-5 bg-[#1e1e1e]/30 border border-[#1e1e1e] rounded-xl space-y-5'>
-            <div className='w-full h-[250px] bg-[#000000] rounded-xl'></div>
+        <div className='w-full p-4 bg-[#1e1e1e]/30 border border-[#1e1e1e] rounded-xl space-y-5'>
+            <div className='w-full h-[260px] bg-[#000000] rounded-xl relative'>
+                <Image
+                    src={course?.image}
+                    alt={course?.title}
+                    fill={true}
+                    priority={true}
+                    className='object-cover rounded-xl'
+                />
+                <div className='absolute inset-0 bg-[#000000]/10 flex items-end justify-end p-4'>
+                    <Badge variant='outline'>{course?.category}</Badge>
+                </div>
+            </div>
             <div className='space-y-2'>
-                <h3 className='text-xl font-bold text-[#ffffff] lg:text-2xl'>
-                    React.js Architecture & Design Patterns
-                </h3>
+                <div className='w-full min-h-[65px]'>
+                    <h3 className='text-xl font-bold text-[#ffffff] lg:text-2xl'>
+                        {course?.title}
+                    </h3>
+                </div>
                 <div className='flex items-center justify-between'>
-                    <p className='text-base text-[#A3A3A3]'>Rakibul Rahman</p>
+                    <p className='text-base text-[#A3A3A3]'>
+                        {course?.instructor}
+                    </p>
                     <div className='flex items-center justify-center gap-2'>
                         <Calendar size='xs' color='#ffffff' />
                         <span className='text-base text-[#ffffff] mt-0.5'>
-                            March 13, 2026
+                            {course?.date &&
+                                format(
+                                    new Date(`${course?.date}`),
+                                    'MMMM dd, yyyy',
+                                )}
                         </span>
                     </div>
                 </div>
--- src/components/ui/TrendingCoursesList/TrendingCoursesList.jsx
+++ src/components/ui/TrendingCoursesList/TrendingCoursesList.jsx
@@ -1,11 +1,16 @@
+import { getCourses } from '@/lib/get-courses';
 import TrendingCard from '../TrendingCard/TrendingCard';
 
-const TrendingCoursesList = () => {
+const TrendingCoursesList = async () => {
+    const { courses } = await getCourses(
+        'https://skillsphere-rrdev.vercel.app/api/courses',
+    );
+
     return (
         <div className='w-full max-w-[1600px] mx-auto grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3'>
-            <TrendingCard />
-            <TrendingCard />
-            <TrendingCard />
+            {courses.slice(6, 9).map((course, idx) => (
+                <TrendingCard key={idx} course={course} />
+            ))}
         </div>
     );
 };
